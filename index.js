b75c15817c9882092cd33b5acc574dcb4e3d391b
Łukasz Mierzwa <l.mierzwa@gmail.com>
2021-05-15 17:39:38 +0100

    don't use npx

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require("@actions/core");
 const execa = require("execa");
 
-let args = ["depcheck"];
+let args = ["./node_modules/.bin/depcheck"];
 
 const config = core.getInput("config");
 if (config !== "") {
@@ -16,7 +16,7 @@ args.push(workdir);
 (async () => {
   core.info(`Running: ${args.join(" ")}`);
   try {
-    const { stdout } = await execa("npx", args);
+    const { stdout } = await execa("node", args);
     core.info(stdout);
   } catch (err) {
     core.setFailed(err.stdout);
